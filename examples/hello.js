964b0c8d505ad2571fc6b655c792022b7a9b125e
Arnaud Leymet <arnaud.leymet@gmail.com>
2016-08-17 13:20:47 +0200

    Standard JS code formatting

--- examples/hello.js
+++ examples/hello.js
@@ -1,15 +1,15 @@
 // Prerequisites
-const chalk = require('chalk');
+const chalk = require('chalk')
 
 // Initialization
-const BotML = require('../lib/botml');
-let bot = new BotML('./examples/hello.bot');
+const BotML = require('../lib/botml')
+let bot = new BotML('./examples/hello.bot')
 
 // Capture all events
-bot.on('*', (event, ...args) => console.log(chalk.dim('Received event'), event, chalk.dim(JSON.stringify(args))));
+bot.on('*', (event, ...args) => console.log(chalk.dim('Received event'), event, chalk.dim(JSON.stringify(args))))
 
 // Start the chatbot
-bot.start();
+bot.start()
 
 // Bonus: gracefull terminate the bot on Ctrl-C
-process.on('SIGINT', bot.stop);
+process.on('SIGINT', bot.stop)
