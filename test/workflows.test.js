ccafcab51b730ce1f9be008867cad9c1454b43a6
Arnaud Leymet <arnaud.leymet@gmail.com>
2018-02-25 09:40:42 +0100

    FIX parsing local activators + update tests

--- test/workflows.test.js
+++ test/workflows.test.js
@@ -2,29 +2,57 @@
 const { assert } = require('chai')
 const { patternify, execPattern } = require('../lib/pattern.js')
 const decache = require('decache')
+// process.env.debug = false
 
 const TEST_CASES = [{
-  file: 'references.bot',
+  file: 'workflows.bot',
   tests: [
     {
       label: 'auto start',
       autostart: true,
-      inputSequence: [],
-      expectedDialogue: `
+      expectedDialogue: toArray`
         < hi
       `
     }, {
-      label: 'reference sub workflows',
-      inputSequence: ['test'],
-      expectedDialogue: `
-        > test
-        < main flow
-        < sub flow 1
-        < sub flow 2
+      label: 'start workflow from a global activator',
+      expectedDialogue: toArray`
+        > start workflow from a global activator
+        < step 1
+        < step 2
+      `
+    }, {
+      label: 'start workflow from a workflow activator',
+      expectedDialogue: toArray`
+        > start workflow from a workflow activator
+        < step 1
+        < step 2
+      `
+    }, {
+      label: 'do not start a workflow from an invalid activator',
+      expectedDialogue: toArray`
+        > something else
+        < catch
+      `
+    }, {
+      label: 'complete a workflow',
+      expectedDialogue: toArray`
+        > start workflow from a workflow activator
+        < step 1
+        < step 2
+        > whatever
+        < step 3
+        > this
+        < step 4
+        < step 5
+        < step 6
       `
     }
   ]
 }]
+
+function toArray (text) {
+  return text.toString().split(/\n/).map(l => l.trim()).filter(l => l)
+}
 
 describe('Workflows', function () {
   TEST_CASES.forEach(testCase => {
@@ -40,11 +68,13 @@ describe('Workflows', function () {
             dialogue.push(`< ${reply}`)
           })
           if (test.autostart) bot.start()
-          test.inputSequence.forEach(input => {
+          const expectedDialogue = test.expectedDialogue
+          const inputSequence = expectedDialogue.filter(l => l.match(/^>/)).map(l => l.replace(/^>\s*/, ''))
+          inputSequence.forEach(input => {
             dialogue.push(`> ${input}`)
             bot.send(input)
           })
-          assert.equal(dialogue.join('').toLowerCase(), test.expectedDialogue.split(/\n/).filter(l => l).map(l => l.trim()).join(''))
+          assert.deepEqual(dialogue.map(l => l.toLowerCase()), expectedDialogue.map(l => l.toLowerCase()))
         })
       })
     })
