df72a1d0c1fe72feaa1f7ffd08f83635c2bb6bf1
Arnaud Leymet <arnaud.leymet@gmail.com>
2018-02-26 20:16:59 +0100

    FIX make code evaluation more robust

--- test/code.test.js
+++ test/code.test.js
@@ -1,48 +1,34 @@
 /* eslint-env mocha */
 const { assert } = require('chai')
-const { runCustomTests, toArray } = require('./base')
+const { runDialogueTests } = require('./base')
 
-runCustomTests('code', [{
+runDialogueTests('code', [{
   file: 'code.bot',
   tests: [
+    // {
+    //   label: 'run a code line and play with variables',
+    //   expectedDialogue: `
+    //     > start who
+    //     < first name?
+    //     > john
+    //     < last name?
+    //     > doe
+    //     < Ok, I will call you "dear john doe"\\nand what is your email?
+    //     > john@doe.com
+    //     < your email validity: true
+    //   `
+    // },
     {
-      label: 'run a code line and play with variables',
+      label: 'run a code that relies on context',
       expectedDialogue: `
-        > start
-        < first name?
-        > john
-        < last name?
-        > doe
-        < Ok, I will call you "dear john doe"\\nand what is your email?
-        > john@doe.com
-        < your email validity: true
+        > start using context
+        < let's start\\ngive me a number
+        > 9
+        < psst!
+        < here!
+        < did you just hear that?
+        < You just gave me the number 9
       `
     }
   ]
-}], (test, bot) => {
-  const dialogue = []
-  const variables = new Map()
-  bot.on('reply', reply => {
-    dialogue.push(`< ${reply}`)
-  })
-  bot.on('setvar', str => {
-    const [key, value] = str.split('=')
-    let val = value
-    if (value && value[0] === '$') {
-      if (value.match(/^\$\d*$/) && bot.context.variables.has(val)) {
-        val = bot.context.variables.get(val)
-      } else if (bot.context.variables.has(value.replace(/^\$/, ''))) {
-        val = bot.context.variables.get(value.replace(/^\$/, ''))
-      }
-    }
-    bot.context.variables.set(key, val)
-  })
-  if (test.autostart) bot.start()
-  const expectedDialogue = toArray(test.expectedDialogue)
-  const inputSequence = expectedDialogue.filter(l => l.match(/^>/)).map(l => l.replace(/^>\s*/, ''))
-  inputSequence.forEach(input => {
-    dialogue.push(`> ${input}`)
-    bot.send(input)
-  })
-  assert.deepEqual(dialogue.map(l => l.toLowerCase()), expectedDialogue.map(l => l.toLowerCase()))
-})
+}])
