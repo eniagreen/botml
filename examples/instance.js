7c0c78afb805ee635c644e29c2a95e54f184130f
Totomakers <totomakers@gmail.com>
2018-03-07 15:00:00 +0100

    :bug: Adding missing context + example for testing

---
+++ examples/instance.js
@@ -0,0 +1,8 @@
+// Prerequisites
+const chalk = require('chalk')
+
+// Initialization
+const BotML = require('../lib/botml')
+
+let bot = new BotML('./examples/service.bot')
+let bot2 = new BotML('./examples/service.bot')
