45b4ff29a62bfc5d556aa96fc1f4f69379a45b1b
Arnaud Leymet <arnaud.leymet@gmail.com>
2018-12-02 17:06:01 +0100

    FIX runkit example

--- examples/runkit.js
+++ examples/runkit.js
@@ -1,4 +1,4 @@
 const SCRIPT = 'https://raw.githubusercontent.com/codename-co/botml/master/examples/hello.bot'
-const Botml = require('botml')
+const Botml = require('botml/lib/botml')
 const bot = new Botml(SCRIPT)
-bot.start()
+bot.start(true)
